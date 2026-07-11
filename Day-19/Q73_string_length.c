9a3415bb1c70faa56d80124ef524ca5e769ea4c9
sagarrana051143-ship-it <sagar.rana051143@gmail.com>
2026-07-11 21:45:10 +0530

    Add string length calculation program in C
    Implement a C program to calculate string length.

---
+++ Day-19/Q73_string_length.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[100];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    printf("Length of string = %lu\n", strlen(str) - 1);
+
+    return 0;
+}
