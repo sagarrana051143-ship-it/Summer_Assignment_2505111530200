db3656a1fbf0d6e4d0cd381e6cd50931e21d24fd
sagarrana051143-ship-it <sagar.rana051143@gmail.com>
2026-07-11 20:30:43 +0530

    Add program to check for Strong Numbers

---
+++ Day-5/Q18_strong_number.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, temp, digit;
+    int sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    temp = num;
+
+    while(temp != 0)
+    {
+        digit = temp % 10;
+
+        int fact = 1;
+        for(int i = 1; i <= digit; i++)
+        {
+            fact *= i;
+        }
+
+        sum += fact;
+        temp /= 10;
+    }
+
+    if(sum == num)
+        printf("%d is a Strong Number.\n", num);
+    else
+        printf("%d is NOT a Strong Number.\n", num);
+
+    return 0;
+}
