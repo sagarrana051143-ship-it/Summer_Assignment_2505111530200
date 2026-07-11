51146c0ae9d0cb54e5c7d5a95c4b7d74bb154f93
sagarrana051143-ship-it <sagar.rana051143@gmail.com>
2026-07-11 20:24:36 +0530

    Add GCD calculation program in Q11_gcd.c
    Implement a program to calculate the GCD of two numbers.

---
+++ Day-3/Q11_gcd.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, gcd, i;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    gcd = 1;
+
+    for (i = 1; i <= a && i <= b; i++)
+    {
+        if (a % i == 0 && b % i == 0)
+        {
+            gcd = i;
+        }
+    }
+
+    printf("GCD = %d\n", gcd);
+
+    return 0;
+}
