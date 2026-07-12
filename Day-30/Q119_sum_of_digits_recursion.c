66b0254d5755a6729faed70c920c9e92a998fc70
sagarrana051143-ship-it <sagar.rana051143@gmail.com>
2026-07-12 10:53:12 +0530

    Add recursive function to sum digits of a number

---
+++ Day-30/Q119_sum_of_digits_recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int sumOfDigits(int n)
+{
+    if(n == 0)
+        return 0;
+
+    return (n % 10) + sumOfDigits(n / 10);
+}
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Sum of Digits = %d\n", sumOfDigits(num));
+
+    return 0;
+}
